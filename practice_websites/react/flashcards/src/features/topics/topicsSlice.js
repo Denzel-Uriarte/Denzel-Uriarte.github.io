068c75b282e925b5024fca870fef91edfa877e4f
Denzel Uriarte <denzeluriartem@gmail.com>
2023-04-03 16:45:16 -0700

    uploaded react flashcards site

---
+++ practice_websites/react/flashcards/src/features/topics/topicsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, } from '@reduxjs/toolkit';
+
+
+export const topicsSlice = createSlice({
+	name: 'topics',
+	initialState: {
+		topics: {},
+	},
+	reducers: {
+		addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics[id] = {
+        id: id,
+        name: name,
+        icon,
+        quizIds: []
+      };
+    },
+		addQuizIdForTopic: (state, action) => {
+			const { quizId, topicId } = action.payload;
+      state.topics[topicId].quizIds.push(quizId);
+		}
+	}
+})
+
+export const { addTopic, addQuizIdForTopic } = topicsSlice.actions;
+export const selectTopics = (state) => state.topics.topics;
+export default topicsSlice.reducer;
